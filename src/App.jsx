dd114179a46068055eaa8e3b3f5a74e48a04bedb
Multsentender <600344dima1221@gmail.com>
2021-10-14 15:29:30 +0500

    to laptop

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import {Header} from './components'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-container">
+        <div className="container">
+          <h2>В 2021 году великий маг Всеросии увёл войско за ворота.</h2>
+        </div>
+      </div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Link, animateScroll as scroll} from 'react-scroll'
+
+import logo from '../assets/icon/logo.svg'
+
+const Header = () => {
+    const scrollToTop = () => scroll.scrollToTop();
+
+    return (
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <img src={logo} alt="Viva-logotype" />
+                    <h1>Вива Металл</h1>
+                </div>
+                <ul className="nav">
+                    <li className="nav--item" onClick={scrollToTop}>Главная</li>
+                    <li className="nav--item">
+                        <Link 
+                            to="services"
+                            smooth={true}
+                            duration={550}>Услуги
+                        </Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link 
+                            to="contacts"
+                            smooth={true}
+                            duration={550}>Контакты
+                        </Link>
+                    </li>
+                </ul>
+                <div className='phone'>+7 (922) 888 05 55</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
